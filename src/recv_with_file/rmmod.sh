0ef8a7eff983842d8e7b62dce09faca6c5bccc63
abiram1985 <abiram@neominds.in>
2015-06-01 19:01:38 +0530

    remove files for re-structuring svn

--- src/recv_with_file/rmmod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-sudo rmmod pull_ring_buf.ko
-sudo rmmod pca_tdma_code.ko
-sudo rmmod test.ko
-sudo rmmod ring_buf.ko
-sudo rmmod uart_driver_x86.ko
-sudo dmesg > ./recvmsg
--- src/send_with_file/insmod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo dmesg -C
-sudo insmod ring_buf.ko
-sudo insmod uart_driver_x86.ko
-sudo insmod HIP.ko
-sudo insmod post_ring_buf.ko
-sudo insmod pca_tdma_code.ko
-
-
